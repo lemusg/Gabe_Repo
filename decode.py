26caadad4968959655a43d869c03fbf8d06c4edc
Sebastian Dion <sebastiandion@Sebastians-MacBook-Pro.local>
2023-10-24 14:08:44 -0400

    sebby decode

---
+++ decode.py
@@ -0,0 +1,12 @@
+def decode(password):
+    decopass = ''
+    for i in password:
+        if int(i) -3 == -3:
+            decopass += '7'
+        elif int(i) - 3 == -2:
+            decopass += '8'
+        elif int(i) - 3 == -1:
+            decopass += '9'
+        else:
+            decopass += str((int(i) - 3))
+    return decopass
