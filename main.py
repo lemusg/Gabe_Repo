d3f2577bfae7d54a7e4736b1cc2c1d52a7369ef5
Gabriel Lemus <lemusg@ufl.edu>
2023-10-24 15:08:48 -0400

    updated main.py

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
-# Gabriel Lemus
-def main():
-    pass
+from Labs.encode import encode
+from Labs.decode import decode
 
+
+def main():
+    print('Menu')
+    print('-------------')
+    print('1. Encode')
+    print('2. Decode')
+    print('3. Quit')
+    print()
+    user = input('Please enter an option: ')
+    return int(user)
+
+
+password = ''
 
 if __name__ == '__main__':
-    main()
+    while True:
+        user_input = main()
+        if user_input == 1:
+            password = input('Please enter your password to encode: ')
+            password = encode(password)
+            print('Your password has been encoded and stored!')
+            print()
+        elif user_input == 2:
+            print(f'The encoded password is {password}, and the original password is {decode(password)}.')
+            print()
+        elif user_input == 3:
+            exit()
+        else:
+            continue
